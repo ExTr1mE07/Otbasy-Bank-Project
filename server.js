fb5e3789c85787734b237fba3d1f81c545fe54ed
Alpysbayev Tamyrlan <ExTr1mE07>
2024-06-10 01:20:27 +0600

    almost end

--- server.js
+++ server.js
@@ -6,9 +6,53 @@ app.use(express.static(__dirname + '/public'))
 
 app.set("view engine", "ejs")
 
-app.get('/' , (req , res) => {
-    res.render("index")
+
+
+app.get('/page0' , (req , res) => {
+    res.render("page-0")
 })
+app.get('/page1' , (req , res) => {
+    res.render("page-1")
+})
+
+app.get('/page2' , (req , res) => {
+    res.render("page-2")
+})
+
+app.get('/page3' , (req , res) => {
+    res.render("page-3")
+})
+
+app.get('/page4' , (req , res) => {
+    res.render("page-4")
+})
+
+app.get('/page5' , (req , res) => {
+    res.render("page-5")
+})
+
+app.get('/page6' , (req , res) => {
+    res.render("page-6")
+})
+
+app.get('/page7' , (req , res) => {
+    res.render("page-7")
+})
+
+app.get('/page8' , (req , res) => {
+    res.render("page-8")
+})
+
+app.get('/page9' , (req , res) => {
+    res.render("page-9")
+})
+
+app.get('/page10' , (req , res) => {
+    res.render("page-10")
+})
+
+
+
 
 const PORT = 8000;
 app.listen(PORT, () => {
